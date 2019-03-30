c735e343670d32f6793cb7fb127b63d12bcc3fdd
Don Nguyen <ndon55555@gmail.com>
2019-03-29 23:42:53 -0400

    Add folder containing my personal configurations of programs. Fix cleanup

---
+++ configurations/.vimrc
@@ -0,0 +1,11 @@
+" NOTE: Comments in Vim are denoted with a double quote.
+set background=dark
+set number " Show line numbers
+set hlsearch " Highlight search
+:highlight lineNr ctermfg=white " Make line numbers white
+set autoindent
+   
+" Tab size = 4
+set tabstop=4
+set shiftwidth=4
+set expandtab
--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+setupDir="/tmp/setup-tools-tmp"
 green='\033[0;32m'
 noColor='\033[0m'
 
@@ -18,48 +19,30 @@ cleanup () {
     action "Removing unused packages"
     apt autoremove -f
     action "Removing temporary folder for setting up tools"
-    rm -rf setupDir
+    rm -rf $setupDir
 }
 
 setup () {
-    setupDir="/tmp/setup-tools-tmp"
     action "Creating temporary folder for setting up tools at $setupDir"
     mkdir "$setupDir"
-    cd "$setupDir"
 
     action "Installing zsh and oh-my-zsh"
     apt install zsh
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-    sed -i -e 's/ZSH_THEME="robbyrussell"/ZSH_THEME="af-magic"/' ~/.zshrc # TODO somehow change ZSH_THEME="robbyrussell"
-                                                                          # to ZSH_THEME=".*"
 
-    action "Adding vim configurations"
-cat > ~/.vimrc <<- VimConfig
-" NOTE: Comments in Vim are denoted with a double quote.
-set background=dark
-set number " Show line numbers
-set hlsearch " Highlight search
-:highlight lineNr ctermfg=white " Make line numbers white
-set autoindent
-   
-" Tab size = 4
-set tabstop=4
-set shiftwidth=4
-set expandtab
-VimConfig
+    action "Replacing vim configurations"
+    cp "$(dirname $0)"/configurations/.vimrc ~/.vimrc
 
-    action "Adding tmux configurations"
-cat > ~/.tmux.conf <<- TMUXConfig
-set -g mouse on
-TMUXConfig
+    action "Replacing tmux configurations"
+    cp "$(dirname $0)"/configurations/.tmux.conf ~/.tmux.conf
 
     action "Making sure Git and other programs use vim as default editor"
     export VISUAL="vim"
     export EDITOR="$VISUAL"
 
     action "Installing fuzzy finder"
-    git clone --depth 1 https://github.com/junegunn/fzf.git
-    ./fzf/install --all
+    git clone --depth 1 https://github.com/junegunn/fzf.git "$setupDir"/fzf
+    "${setupDir}"/fzf/install --all
 }
 
 ################## Script execution ##################
