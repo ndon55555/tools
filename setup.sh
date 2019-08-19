9e426320a16556f95b06ee2edf98901e2c97d77e
Don Nguyen <ndon55555@gmail.com>
2019-08-18 22:53:13 -0400

    Only run sudo when needed

--- setup.sh
+++ setup.sh
@@ -11,16 +11,9 @@ action () {
     echo -e "${green}TOOLS SETUP:${noColor} $1..."
 }
 
-elevate_privileges () {
-    if [ $EUID != 0 ]; then
-        sudo "$0" "$@"
-        exit $?
-    fi
-}
-
 cleanup () {
     action "Removing unused packages"
-    apt autoremove -f -y
+    sudo apt autoremove -f -y
     action "Removing temporary folder for setting up tools"
     rm -rfv $setupDir
 }
@@ -30,13 +23,13 @@ setup () {
     action "Created temporary folder for setting up tools at $setupDir"
 
     action "Running apt-get update"
-    apt-get -y update
+    sudo apt-get -y update
 
     action "Installing curl, wget, python3, and vim"
-    apt-get -y install curl wget python3 vim
+    sudo apt-get -y install curl wget python3 vim
 
     action "Installing zsh and oh-my-zsh"
-    apt-get -y install zsh
+    sudo apt-get -y install zsh
     RUNZSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
     action "Replacing zsh configurations"
@@ -59,44 +52,43 @@ setup () {
     popd
 
     action "Installing The Silver Searcher"
-    apt-get -y install silversearcher-ag
+    sudo apt-get -y install silversearcher-ag
 
     action "Installing Golang 1.12.9"
     wget -O "$setupDir/golang.tar.gz" https://dl.google.com/go/go1.12.9.linux-amd64.tar.gz
-    tar -xzf "$setupDir/golang.tar.gz" -C /usr/local/
+    sudo tar -xzf "$setupDir/golang.tar.gz" -C /usr/local/
 
     action "Installing latest NodeJS"
     pushd "$setupDir"
-    "$scriptsDir/install-latest-node.sh"
+    sudo "$scriptsDir/install-latest-node.sh"
     popd
 
     action "Installing python3-distutils" # Need this to get pip
-    apt-get install -y python3-distutils
+    sudo apt-get install -y python3-distutils
     
     action "Installing latest Pip"
     curl https://bootstrap.pypa.io/get-pip.py -o "$setupDir/get-pip.py"
-    python3 "$setupDir/get-pip.py"
+    sudo python3 "$setupDir/get-pip.py"
 
     action "Installing pynvim" # Makes deoplete plugin for vim work
     pip3 install --user pynvim
 
     action "Installing vim-plug"
-    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    curl -fLo "$HOME/.vim/autoload/plug.vim" --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
     action "Installing vim plugins"
     vim +PlugInstall +qa
 
     action "Ensuring all home files are owned by $user"
-    chown -R "$user" find ~ -maxdepth 1 -name ".*"
+    find "$HOME" -maxdepth 1 -name ".*" | xargs -I {} chown -R "$(whoami)" {} 
 
-    if [[ -z $(ps -p $$ | grep zsh) ]]; then
+    if [[ -z "$(ps -p $$ | grep zsh)" ]]; then
         action "Running zsh"
         ZSH_DISABLE_COMPFIX=true exec zsh -l
     fi
 }
 
 ################## Script execution ##################
-elevate_privileges
 setup
 cleanup
